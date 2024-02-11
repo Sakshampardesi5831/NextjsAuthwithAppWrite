281d447e295fb68375fef3448e5a3a259342dfd8
pardesisaksham <saksham.pardesi@zappian.com>
2024-02-11 17:59:29 +0530

    login and signup with backend integration complete

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,45 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+connect().then(function(){
+  console.log("Database Connected");
+});
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    //check
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        {
+          message: "User Already Exist",
+        },
+        { status: 400 }
+      );
+    }
+    //hashPassword
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+  const newUser=  new User({ username, email, password: hashedPassword });
+  const savedUser=await newUser.save();
+  console.log(savedUser);
+  return NextResponse.json({
+    message:"User Created SuccessFully !!",
+    success:true,
+    savedUser
+  })
+  
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: "Some Went Wrong while Saving",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,26 +1,56 @@
 "use client";
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-
+import { toast } from "react-hot-toast";
 const SignUp = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSignUp = async () => {};
+
+
+  
+  const onSignUp = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Sign Up Success", response.data);
+      toast.success(response.data.message);
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.username.length > 0 &&
+      user.password.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
     <Fragment>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>SignUp</h1>
+        <h1 className="text-white">{loading ? "Processing" : "SignUp"}</h1>
         <hr />
         <label htmlFor="username">username</label>
         <input
           className="border-2 border-black py-1 px-1 mb-4 focus:outline-none
-           focus:border-blue-500 focus:ring-1 focus:ring-blue-500  "
+           focus:border-blue-500 focus:ring-1 focus:ring-blue-500 text-black "
           name="username"
           id="username"
           type="text"
@@ -31,7 +61,7 @@ const SignUp = () => {
         <label htmlFor="email">email</label>
         <input
           className="border-2 border-black py-1 px-1 mb-4 focus:outline-none
-           focus:border-blue-500 focus:ring-1 focus:ring-blue-500  "
+           focus:border-blue-500 focus:ring-1 focus:ring-blue-500  text-black  "
           name="email"
           id="email"
           type="email"
@@ -42,10 +72,10 @@ const SignUp = () => {
         <label htmlFor="password">password</label>
         <input
           className="border-2 border-black py-1 px-1 mb-4 focus:outline-none
-           focus:border-blue-500 focus:ring-1 focus:ring-blue-500  "
+           focus:border-blue-500 focus:ring-1 focus:ring-blue-500  text-black"
           name="password"
           id="password"
-          type="text"
+          type="password"
           placeholder="password"
           value={user.password}
           onChange={(e) => setUser({ ...user, password: e.target.value })}
@@ -55,7 +85,7 @@ const SignUp = () => {
           className="py-2 px-4
        text-white border  border-gray-300 rounded-lg focus:outline-none"
         >
-          Create Account
+          {buttonDisabled ? "No Sign Up" : "Sign Up"}
         </button>
         <Link href={"/login"}>Visit Login Page</Link>
       </div>
